406afe451118077dd6d5f9cef0babbf2a1bbc684
Devfritz <devfritzcode@gmail.com>
2021-06-28 14:52:48 -0400

    add template

---
+++ components/layout/Layout.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Head from 'next/head';
+import Header from './Header';
+import Footer from './Footer';
+
+const Layout = ({ children, title = 'Best Hotels for your Holiday' }) => {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Home from '../components/Home';
 
-export default function Home() {
-  return <div>hello</div>;
+export default function Index() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
